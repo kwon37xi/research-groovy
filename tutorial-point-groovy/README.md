118fc5d1640c88506ad7b46d0335fe0d64459a35
KwonNam Son <kwon37xi@gmail.com>
2016-07-30 01:28:33 +0900

    tutorial-point-groovy basic

---
+++ tutorial-point-groovy/README.md
@@ -0,0 +1,1 @@
+[Tutorial Point - Groovy Tutorial](http://www.tutorialspoint.com/groovy/)
---
+++ tutorial-point-groovy/src/main/groovy/Example.groovy
@@ -0,0 +1,26 @@
+class Example {
+    static void main(String[] args) {
+        println('Hello World')
+
+        // def 로 변수, 함수 등을 정의 할 수 있다.
+        def employeename = "groovy"
+        def $varnamewith$ = "dolars"
+
+        println($varnamewith$)
+    }
+
+    /*
+        다음 클래스들은 무조건 기본 import 된다.
+
+        import java.lang.*
+        import java.util.*
+        import java.io.*
+        import java.net.*
+
+        import groovy.lang.*
+        import groovy.util.*
+
+        import java.math.BigInteger
+        import java.math.BigDecimal
+     */
+}
