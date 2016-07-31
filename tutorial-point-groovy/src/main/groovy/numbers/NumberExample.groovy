6dae615a722369f21889e7ba820f7cba64f560df
KwonNam Son <kwon37xi@gmail.com>
2016-08-01 08:40:42 +0900

    tutorial-point-groovy numbers

---
+++ tutorial-point-groovy/src/main/groovy/numbers/NumberExample.groovy
@@ -0,0 +1,16 @@
+package numbers
+
+
+class NumberExample {
+    static void main(String[] args) {
+
+        double d = 100.675;
+        double e = 100.500;
+        double f = 100.200;
+
+        System.out.println(Math.rint(d));
+        System.out.println(Math.rint(e));
+        System.out.println(Math.rint(f));
+
+    }
+}
