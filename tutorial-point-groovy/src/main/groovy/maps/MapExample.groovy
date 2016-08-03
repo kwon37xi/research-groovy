a395b36aca18d1d78292ce5dfce0c0a6389ef093
KwonNam Son <kwon37xi@gmail.com>
2016-08-03 22:42:28 +0900

    tutorial-point-groovy maps

---
+++ tutorial-point-groovy/src/main/groovy/maps/MapExample.groovy
@@ -0,0 +1,9 @@
+package maps
+
+
+class MapExample {
+    static void main(String[] args) {
+        println(['TopicName' : 'Lists', 'TopicName' : 'Maps']) // only 'Maps' value remains
+        println([:]) // empty map
+    }
+}
