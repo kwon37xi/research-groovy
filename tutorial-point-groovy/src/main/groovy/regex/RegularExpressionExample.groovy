4f3408c4ce26d047c257ec651fa92a68d4379105
KwonNam Son <kwon37xi@gmail.com>
2016-08-04 12:30:55 +0900

    tutorial-point-groovy regex

--- tutorial-point-groovy/src/main/groovy/regex/RegularExpressionExample.groovy
+++ tutorial-point-groovy/src/main/groovy/regex/RegularExpressionExample.groovy
@@ -1,8 +1,15 @@
 package regex
 
-
 class RegularExpressionExample {
     public static void main(String[] args) {
+        def regex = ~'Groovy'
+        println regex.class // java.util.regex.Pattern
+        def matcher = "Groovy" =~ 'Groovy' // =~ 연산자는 정규표현식을 문자열로 받는다.
 
+        println(matcher.class) // java.util.regex.Matcher
+
+        if ('Groovy' =~ 'Groovy') { // 자동으로 boolean으로 변경된다.
+            println ('Groovy matches Groovy')
+        }
     }
 }
