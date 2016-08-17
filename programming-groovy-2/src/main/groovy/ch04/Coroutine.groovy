b29cd5fd9694163365b2306b7ca1d22293fa0c7d
KwonNam Son <kwon37xi@gmail.com>
2016-08-17 12:46:15 +0900

    programming-groovy-2 coroutine

---
+++ programming-groovy-2/src/main/groovy/ch04/Coroutine.groovy
@@ -0,0 +1,16 @@
+package ch04
+
+def iterate(n, closure) {
+    1.upto(n) {
+        println "In iterate with value ${it}"
+        closure(it)
+    }
+}
+
+println "Calling iterate"
+total = 0
+iterate(4) {
+    total += it
+    println "In closure total so far is ${total}"
+}
+println "Done"
