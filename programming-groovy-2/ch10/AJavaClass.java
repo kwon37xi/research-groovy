040cad79bce592af81680f6bb6e5df77e25529e8
KwonNam Son <kwon37xi@gmail.com>
2016-08-26 09:19:19 +0900

    programming-groovy-2 mixing java & groovy

---
+++ programming-groovy-2/ch10/AJavaClass.java
@@ -0,0 +1,9 @@
+public class AJavaClass {
+    {
+        System.out.println("Created Java Class");
+    }
+
+    public void sayHello() {
+        System.out.println("hello");
+    }
+}
---
+++ programming-groovy-2/ch10/UseJavaClass.groovy
@@ -0,0 +1,8 @@
+new AJavaClass().sayHello()
+/*
+-j : joint compilation
+-J : javac options
+groovyc -j AJavaClass.java UseJavaClass.groovy -Jsource 1.6
+java -classpath /home/kwon37xi/.sdkman/candidates/groovy/current/embeddable/groovy-all-2.4.5.jar:. UseJavaClass
+
+ */
