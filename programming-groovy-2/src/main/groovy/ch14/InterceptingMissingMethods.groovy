8d04dcdc32dddfd09432362125314c50d4f6aa2c
KwonNam Son <kwon37xi@gmail.com>
2016-08-31 13:01:45 +0900

    programming-groovy-2 intercepting missing methods - GroovyInterceptable

--- programming-groovy-2/src/main/groovy/ch14/InterceptingMissingMethods.groovy
+++ programming-groovy-2/src/main/groovy/ch14/InterceptingMissingMethods.groovy
@@ -8,5 +8,38 @@ class PlayPerson3 implements GroovyInterceptable {
 
     def plays = ['Tennis', 'VolleyBall', 'BasketBall']
 
+    def invokeMethod(String name, args) {
+        System.out.println "intercepting call for $name"
+
+        def method = metaClass.getMetaMethod(name, args)
+
+        if (method) {
+            method.invoke(this, args)
+        } else {
+            // 원본 invokeMethod 호출. 연쇄적으로 methodMissing 호출.
+            metaClass.invokeMethod(this, name, args)
+        }
+    }
+
+    def methodMissing(String name, args) {
+        System.out.println "methodMissing called for $name"
+        def methodInList = plays.find { it == name.split('play')[1]}
+
+        if (methodInList) {
+            def impl = { Object[] vargs ->
+                "playing ${name.split('play')[1]}..."
+            }
+
+            PlayPerson3 instance = this
+            instance.metaClass."$name" = impl // 미래의 요청은 methodMissing을 거치지 않고 메소드에 직접 가게 됨.
+            impl(args)
+        } else {
+            throw new MissingMethodException(PlayPerson3.class, args)
+        }
+    }
 }
 
+jack = new PlayPerson3()
+println jack.work()
+println jack.playTennis()
+println jack.playTennis()
