e479e807ff70a04f8d2a0a8b4dd25b2d6597d4e7
KwonNam Son <kwon37xi@gmail.com>
2016-09-11 15:16:15 +0900

    programming-groovy-2 DSL removing parentheses

---
+++ programming-groovy-2/src/main/groovy/ch19/Total.groovy
@@ -0,0 +1,16 @@
+value = 0
+def getClear() { value = 0}
+def add(number) { value += number }
+def getTotal() {
+    println "Total is $value"
+}
+
+clear
+
+add 2
+add 5
+add 7
+
+total
+clear
+total
