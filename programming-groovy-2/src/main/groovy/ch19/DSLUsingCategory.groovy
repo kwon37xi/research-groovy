056a9b9aed91487da5f2cfb224f35d86dc0fb030
KwonNam Son <kwon37xi@gmail.com>
2016-09-11 15:37:13 +0900

    programming-groovy-2 DSL using categories

---
+++ programming-groovy-2/src/main/groovy/ch19/DSLUsingCategory.groovy
@@ -0,0 +1,24 @@
+class DateUtil {
+    static int getDays(Integer self) {
+        self
+    }
+
+    static Calendar getAgo(Integer self) {
+        def date = Calendar.instance
+        date.add(Calendar.DAY_OF_MONTH, -self)
+        date
+    }
+
+    static Date at(Calendar self, Double time) {
+        def hour = (int)(time.doubleValue())
+        def minute = (int)(Math.round((time.doubleValue() - hour) * 100))
+        self.set(Calendar.HOUR_OF_DAY, hour)
+        self.set(Calendar.MINUTE, minute)
+        self.set(Calendar.SECOND, 0)
+        self.time
+    }
+}
+
+use(DateUtil) {
+    println 2.days.ago.at(4.30)
+}
