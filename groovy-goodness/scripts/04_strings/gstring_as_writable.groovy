16baf40d2c2d157da889204cf0885ca00f0d3a6a
KwonNam Son <kwon37xi@gmail.com>
2016-10-03 18:11:25 +0900

    groovy-goodness  GString as Writable

---
+++ groovy-goodness/scripts/04_strings/gstring_as_writable.groovy
@@ -0,0 +1,14 @@
+def data = [
+        new Expando(id: 1, user: 'mrhaki', country: 'The Netherlands'),
+        new Expando(id: 2, user: 'hubert', country: 'The Netherlands')
+]
+
+data.each { userData ->
+    new File("/tmp/${userData.id}.txt").withWriter('UTF-8') { fileWriter ->
+        "User $userData.user lives in $userData.country".writeTo(fileWriter)
+    }
+}
+
+
+assert new File('/tmp/1.txt').text == 'User mrhaki lives in The Netherlands'
+assert new File('/tmp/2.txt').text == 'User hubert lives in The Netherlands'
