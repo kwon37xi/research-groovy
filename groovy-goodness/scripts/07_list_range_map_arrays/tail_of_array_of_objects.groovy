13423a6ce5d9f47bf195c9bdcb751fc8f74af632
KwonNam Son <kwon37xi@gmail.com>
2016-10-15 22:41:44 +0900

    groovy-goodness  head/tail of array of objects

---
+++ groovy-goodness/scripts/07_list_range_map_arrays/tail_of_array_of_objects.groovy
@@ -0,0 +1,7 @@
+String[] stringArray = ['Groovy', 'Grails', 'Spock']
+
+assert 'Groovy' == stringArray.head()
+assert ['Grails', 'Spock'] == stringArray.tail()
+assert 'Groovy' == stringArray.first()
+assert 'Spock' == stringArray.last()
+
