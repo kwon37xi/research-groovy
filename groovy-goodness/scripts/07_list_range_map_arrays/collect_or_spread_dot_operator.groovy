ef5cf4c1a8216d6d2681ac1586b08912a97255f3
KwonNam Son <kwon37xi@gmail.com>
2016-10-17 21:01:38 +0900

    groovy-goodness  spread dot or collect

---
+++ groovy-goodness/scripts/07_list_range_map_arrays/collect_or_spread_dot_operator.groovy
@@ -0,0 +1,14 @@
+// collect() or *.
+
+class User {
+    String name
+    String aloud() { name.toUpperCase() }
+    String toString() { name }
+}
+
+def users = [new User(name: 'mrhaki'), new User(name: 'hubert')]
+
+assert ['mrhaki', 'hubert'] == users*.toString()
+assert ['MRHAKI', 'HUBERT'] == users*.aloud()
+assert ['mrhaki', 'hubert'] == users.collect { it.toString() }
+assert ['MRHAKI', 'HUBERT'] == users.collect { it.aloud() }
