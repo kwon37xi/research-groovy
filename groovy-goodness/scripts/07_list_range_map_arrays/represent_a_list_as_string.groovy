755ac4fa43d50ad7f2a9ddf80d5fd33996dd57ff
KwonNam Son <kwon37xi@gmail.com>
2016-10-23 23:05:05 +0900

    groovy-goodness  represent a list as string

---
+++ groovy-goodness/scripts/07_list_range_map_arrays/represent_a_list_as_string.groovy
@@ -0,0 +1,8 @@
+// toListString()
+def list = ['Groovy', 'Clojure', 'Java']
+
+def listString = list.toListString()
+def abbreviated = list.toListString(12)
+
+assert '[Groovy, Clojure, Java]' == listString
+assert '[Groovy, Clojure, ...]' == abbreviated
