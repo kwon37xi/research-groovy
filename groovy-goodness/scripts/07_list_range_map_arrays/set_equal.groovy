182fc437a75bcacbbbac171470216be4a60ae2a8
KwonNam Son <kwon37xi@gmail.com>
2016-10-24 20:58:41 +0900

    groovy-goodness  set equals

---
+++ groovy-goodness/scripts/07_list_range_map_arrays/set_equal.groovy
@@ -0,0 +1,12 @@
+// same size/same elements = equals
+
+def set1 = ['Java', 42, true] as Set
+def set2 = ['Groovy', 42, true, 'Java'] as Set
+def set3 = [42L, true, 'Java'] as Set
+
+assert set1.equals(set3)
+assert set1 == set3
+assert !set1.equals(set2)
+assert set2 != set3
+
+
