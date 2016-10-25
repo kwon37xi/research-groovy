52ad04afee26818bb1bed5099d9ef8f5b51ffc3e
KwonNam Son <kwon37xi@gmail.com>
2016-10-25 12:45:16 +0900

    groovy-goodness  intersect maps

---
+++ groovy-goodness/scripts/08_map/intersect_map.groovy
@@ -0,0 +1,7 @@
+// intersect : 두 Map에서 key/value가 동일한 것만 뽑아내기.
+
+def m1 = [a: 'Groovy', b: 'rocks', c: '!']
+def m2 = [a: 'Groovy', b: 'rocks', c: '?', d: 'Yes!']
+
+assert [a: 'Groovy', b: 'rocks'] == m1.intersect(m2)
+assert [1: 1.0, 2: 2] == [1: 1.0, 2: 2].intersect([1: 1, 2: 2.0])
