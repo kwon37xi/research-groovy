4bfbf4809991a4fd5f42d9886fedd7e5e614cdb1
KwonNam Son <kwon37xi@gmail.com>
2016-10-26 12:40:19 +0900

    groovy-goodness  map with default value

---
+++ groovy-goodness/scripts/08_map/map_with_defalut_values.groovy
@@ -0,0 +1,11 @@
+// withDefault()
+def m = [start: 'one'].withDefault { key -> key.isNumber() ? 42 : 'Groovy rocks!' }
+
+assert 'one' == m.start
+assert 42 == m['1']
+assert 'Groovy rocks!' == m['I say']
+assert 3 == m.size()
+
+m['mrhaki'] = 'Hubert Klein Ikkink'
+assert 'Hubert Klein Ikkink' == m.mrhaki
+assert 4 == m.size()
