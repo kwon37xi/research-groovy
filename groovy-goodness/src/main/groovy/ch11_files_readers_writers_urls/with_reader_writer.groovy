e43e0797599024079d6e4a39602614f1e08a2d7b
KwonNam Son <kwon37xi@gmail.com>
2016-11-07 13:14:56 +0900

    groovy-goodness  files/readers/writers/urls withReader, withWriter

---
+++ groovy-goodness/src/main/groovy/ch11_files_readers_writers_urls/with_reader_writer.groovy
@@ -0,0 +1,14 @@
+def file = new File('sample.txt')
+file.deleteOnExit()
+
+file.withWriter('UTF-8') {
+    it.writeLine 'Adding this text to the file.'
+}
+
+def s
+file.withReader('UTF-8') {
+    s = it.readLine();
+}
+
+assert 'Adding this text to the file.' == s
+
