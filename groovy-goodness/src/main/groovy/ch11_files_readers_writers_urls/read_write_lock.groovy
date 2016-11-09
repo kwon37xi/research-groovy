2a15b82e518c727e4cc6d7d9ffe31096264ee108
KwonNam Son <kwon37xi@gmail.com>
2016-11-09 12:40:28 +0900

    groovy-goodness  files/readers/writers/urls read/write lock

--- groovy-goodness/src/main/groovy/ch11_files_readers_writers_urls/read_write_lock.groovy
+++ groovy-goodness/src/main/groovy/ch11_files_readers_writers_urls/read_write_lock.groovy
@@ -1,1 +1,45 @@
 // @WithWriteLock, @WithReadLock
+// ReentrantReadWriteLock 대체.
+
+import groovy.transform.*
+
+class Bucket {
+    final def items = []
+
+    @WithWriteLock
+    void add(String item) {
+        items << item
+    }
+
+    @WithReadLock
+    List getAllItems() {
+        items
+    }
+}
+
+def bucket = new Bucket()
+
+def allThreads = []
+
+3.times {
+    allThreads << Thread.start {
+        5.times {
+            bucket.add Thread.currentThread().name + ': Groovy rocks'
+            println "> ${Thread.currentThread().name} adds item to bucket."
+            sleep 100
+        }
+    }
+
+    allThreads << Thread.start {
+        3.times {
+            int numberOfItems = bucket.allItems.size()
+            println "< ${Thread.currentThread().name} says $numberOfItems items are in the bucket."
+            sleep 150
+        }
+    }
+}
+
+allThreads.each { it.join() }
+
+assert bucket.items.size() == 15
+println bucket.items
