47f1b664abf568beb770807ae3c36c985239e6ad
KwonNam Son <kwon37xi@gmail.com>
2016-11-14 12:19:02 +0900

    groovy-goodness  xml/json MarkupTemplateEngine

--- groovy-goodness/scripts/12_xml_json/markup_template_engine.groovy
+++ groovy-goodness/scripts/12_xml_json/markup_template_engine.groovy
@@ -22,7 +22,37 @@ String list = """ul {
         li item
     }
 }
+"""
 
 // Use fragment method.
-fragment
+fragment list, items: ['A', 'B', 'C']
+
+ul {
+    ['cloud', 'home', 'pencil'].each {  iconValue ->
+        // Generate output with predefined
+        // fragment faIcon. Pass value for model attribute icon.
+        // We must use ${stringO{...}} because we apply
+        // the fragment as inline element.
+        li "${stringOf {fragment(faIcon, icon:iconValue)}}"
+    }
+}
+
+// Reuse fragment in other parts of the template.
+p "This is a ${stringOf {fragment(faIcon, icon: 'cog')}} settings icon."
 ''')
+
+Writer writer = new StringWriter()
+Writable output = template.make([:])
+output.writeTo(writer)
+String result = writer.toString()
+println result
+def expected = $/\
+<ul><li>A</li><li>B</li><li>C</li></ul>\
+<ul>\
+<li><span class="fa fa-cloud"></span></li>\
+<li><span class="fa fa-home"></span></li>\
+<li><span class="fa fa-pencil"></span></li>\
+</ul>\
+<p>This is a <span class="fa fa-cog"></span> settings icon.</p>\
+/$
+assert result == expected
