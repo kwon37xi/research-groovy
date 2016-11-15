3426d572e43135990ea2ebc1fbc2b5e81c3a26ae
KwonNam Son <kwon37xi@gmail.com>
2016-11-15 12:31:47 +0900

    groovy-goodness  xml/json JsonSlurper JsonParserType.LAX

---
+++ groovy-goodness/scripts/12_xml_json/json_parse_wicked_json.groovy
@@ -0,0 +1,27 @@
+// 미묘하게 비표준인 JSON 파싱.
+// JsonParseType.LAX
+
+import groovy.json.*
+import static groovy.json.JsonParserType.LAX as RELAX
+
+def jsonText = '''
+{
+    message: {
+        /* Set header of message */
+        header: {
+            "from": 'mrhaki',
+            'to': ["Groovy Users", "Java Users"]
+        }
+        # Include a little body for the message
+        'body': "Check out Groovy's gr8 JSON support."
+    }
+}
+'''
+
+def json = new JsonSlurper().setType(RELAX).parseText(jsonText)
+
+def header = json.message.header
+assert header.from == 'mrhaki'
+assert header.to[0] == 'Groovy Users'
+assert header.to[1] == 'Java Users'
+assert json.message.body == "Check out Groovy's gr8 JSON support."
