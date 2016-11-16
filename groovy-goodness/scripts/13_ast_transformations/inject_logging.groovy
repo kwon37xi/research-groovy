23fe88cb8dd2e44077fc4a8c86e9ed62ab916337
KwonNam Son <kwon37xi@gmail.com>
2016-11-16 14:47:10 +0900

    groovy-goodness  AST Transformation @Logging

---
+++ groovy-goodness/scripts/13_ast_transformations/inject_logging.groovy
@@ -0,0 +1,8 @@
+// @Slf4j
+// 또한 모든 로깅은 로깅 레벨이 맞는지 선검사가 이뤄진다.
+
+// 클래스에 @Sfl4j 애노테이션을 붙이고서 log 객체를 사용하면 됨.
+// java.util.logging : @Log
+// Log4j : @Log4j
+// Commons Logging : @Commons
+// Slf4j ; @Slf4j
